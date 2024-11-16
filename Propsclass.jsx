1226d57d033bc61983dc0791e2d69cdcfc74937a
anshiahlawat <155559499+anshiahlawat@users.noreply.github.com>
2024-11-16 10:28:40 +0530

    Update Propsclass.jsx

--- Propsclass.jsx
+++ Propsclass.jsx
@@ -1,17 +1,3 @@
-// import React, { Component } from 'react'
 
-// export default class Propsclass extends Component {
-//   render() {
-//     const {name,email}=this.props;
-//     return (
-//       <>
-//       <h1>user list </h1>
-//       {/* <p>{this.props.name}</p> */}
-//       <p>{name}</p>
-//       <p>{email}</p>
-//       </>
-//     )
-//   }
-// }
 
 
