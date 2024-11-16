ce9e01c5ac2ef293d84ce14955a3f17756571ca9
anshiahlawat <155559499+anshiahlawat@users.noreply.github.com>
2024-11-16 10:28:12 +0530

    Update Usercard.jsx

--- Usercard.jsx
+++ Usercard.jsx
@@ -1,16 +1,1 @@
-// import React from 'react'
 
-// export default function Usercard(props) {
-//     const {name, age, email}=props;
-//     console.log('Usercard props:', props);
-//   return (
-    
-//     <>
-//     <h1>hello {name}</h1>
-//     <h2>Name: {name}</h2>
-//     <p>age: {age}</p>
-//     <p>email: {email}</p>
-//     </>
-    
-//   )
-// }
